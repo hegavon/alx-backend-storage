e90c93e4a05c7b20e9a57837216db2252c694d79
Victor Amajuoyi <victorgavon@gmail.com>
2024-07-11 02:05:34 +0100

    My SQL Advanced - 3. Old school band

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,12 +1,5 @@
--- List all bands with Glam rock as their main style, ranked by their longevity
--- The table dump metal_bands.sql is assumed to be imported into the database
-
-SELECT 
-    band_name,
-    CASE 
-        WHEN split IS NULL THEN 2022 - formed
-        ELSE split - formed
-    END AS lifespan
-FROM metal_bands
-WHERE main_style = 'Glam rock'
-ORDER BY lifespan DESC;
+-- This SQL script select band_name, and lifespan column which is difference
+SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
+    FROM metal_bands
+    WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
+    ORDER BY lifespan DESC;
