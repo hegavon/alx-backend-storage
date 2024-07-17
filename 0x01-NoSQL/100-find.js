f4f05deb7df62a2a10b38b7c65aa8cd24f143c2c
Victor Amajuoyi <victorgavon@gmail.com>
2024-07-17 15:45:52 +0100

    No SQL - Task 13. Regex filter

---
+++ 0x01-NoSQL/100-find.js
@@ -0,0 +1,4 @@
+// 100-find.js
+// This script lists all documents with names starting by "Holberton" in the "school" collection
+
+db.school.find({ name: { $regex: /^Holberton/ } }).forEach(printjson);
