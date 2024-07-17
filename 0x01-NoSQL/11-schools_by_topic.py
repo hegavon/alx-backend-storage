0fb2d5f56fa23fd8e111ecaffd862eccbceb0fcd
Victor Amajuoyi <victorgavon@gmail.com>
2024-07-17 14:13:07 +0100

    No SQL - Task 11. Where can I learn Python?

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+'''Module to find schools by a specific topic.'''
+
+
+def schools_by_topic(mongo_collection, topic):
+    '''Returns the list of schools having a specific topic.
+
+    Args:
+        mongo_collection: The pymongo collection object.
+        topic (str): The topic to search for.
+
+    Returns:
+        List of dictionaries representing the schools that have the given topic
+    '''
+    return list(mongo_collection.find({"topics": topic}))
