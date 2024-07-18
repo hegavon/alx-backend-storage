8e1bae06cc61d660c9cef4f4566a6db60ff892fa
Victor Amajuoyi <victorgavon@gmail.com>
2024-07-18 02:27:28 +0100

    Redis Basic - Task 0. Writing strings to Redis

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+Cache class for storing data in Redis.
+"""
+import redis
+import uuid
+from typing import Union
+
+
+class Cache:
+    def __init__(self):
+        """Initialize Cache class with Redis client & flush the database."""
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """
+        Store data in Redis using a random key.
+
+        Args:
+            data (Union[str, bytes, int, float]): The data to store.
+
+        Returns:
+            str: The key under which the data is stored.
+        """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
+
+
+# Example usage
+if __name__ == "__main__":
+    cache = Cache()
+
+    data = b"hello"
+    key = cache.store(data)
+    print(key)
+
+    local_redis = redis.Redis()
+    print(local_redis.get(key))
